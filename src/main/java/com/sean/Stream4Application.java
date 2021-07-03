380b2e48426b9cee4403e65cee74baa848e99abc
Sean Maxwell <sa.maxwell@yahoo.com>
2021-07-03 14:01:21 -0500

    Initial commit - working stream with one processor

---
+++ src/main/java/com/sean/Stream4Application.java
@@ -0,0 +1,23 @@
+package com.sean;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Stream4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stream4Application.class, args);
+    }
+
+    @Bean
+    public java.util.function.Consumer<KStream<String, String>> process() {
+        return input -> input.foreach((k,v) -> {
+            System.out.println("\t" + k + " --> " + v);
+        });
+
+    }
+
+}
---
+++ src/test/java/com/sean/Stream4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stream4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
